38dcd8d44918966ba1732c0dc1babd951535d120
Matteo Cancellieri <canc3l@gmail.com>
2019-10-03 14:58:24 +0100

    simple midi

--- shepherd_tone.py
+++ shepherd_tone.py
@@ -10,12 +10,11 @@ from time import sleep
 class ShepherdMusic:
     volume_increment_u=0
     volume_increment_d=0
-    def sheperd_tone(self, notes_i, length=0.5):
+    def sheperd_tone(self, notes_i, length=0.5, instrument_to_play=instrument.Piano()):
 
-        intrument_to_play = instrument.Piano()
         # Highest octave, volume gets lower
         shepard_tone_u = stream.Part()
-        shepard_tone_u.insert(0, intrument_to_play)
+        shepard_tone_u.insert(0, instrument_to_play)
         c_major = ['C#5', 'D#5', 'E#5', 'F#5', 'G#5', 'A#5', 'B#5', 'C#6', 'D#6', 'E#6', 'F#6', 'G#6', 'A#6', 'B#6']
         c_major = [c_major[notes_i%len(c_major)]]
 
@@ -27,7 +26,7 @@ class ShepherdMusic:
 
         # Middle octave, volume constant
         shepard_tone_m = stream.Part()
-        shepard_tone_m.insert(0, intrument_to_play)
+        shepard_tone_m.insert(0, instrument_to_play)
         c_major = ['C#3', 'D#3', 'E#3', 'F#3', 'G#3', 'A#3', 'B#3', 'C#4', 'D#4', 'E#4', 'F#4', 'G#4', 'A#4', 'B#4']
         c_major = [c_major[notes_i%len(c_major)]]
 
@@ -37,7 +36,7 @@ class ShepherdMusic:
 
         # Lowest octave, volume gets higher
         shepard_tone_l = stream.Part()
-        shepard_tone_l.insert(0, intrument_to_play)
+        shepard_tone_l.insert(0, instrument_to_play)
         c_major = ['C#1', 'D#1', 'E#1', 'F#1', 'G#1', 'A#1', 'B#1', 'C#2', 'D#2', 'E#2', 'F#2', 'G#2', 'A#2', 'B#2']
         c_major = [c_major[notes_i%len(c_major)]]
 
@@ -54,7 +53,7 @@ class ShepherdMusic:
         player.play()
 
     def play_gameover(self):
-        player = StreamPlayer(self.sheperd_tone(0, 1))
+        player = StreamPlayer(self.sheperd_tone(0, 1, instrument.Trombone()))
         player.play()
 
 
