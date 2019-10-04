b1b69076c8b5c24c3047f5a012840c6e619351f6
Matteo Cancellieri <canc3l@gmail.com>
2019-10-04 14:19:30 +0100

    restore_logo

--- main.py
+++ main.py
@@ -103,7 +103,6 @@ class MakerMasher():
     def write_text_on_screen(self, text):
         print(self.off_rect)
         print(self.off_text_surface)
-        self.screen.fill((0, 0, 0))
         text_lines = text.split("\n")
         lines = 0
         for line in text_lines:
